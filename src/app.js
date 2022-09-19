299e91ee828beb48bdfaa65fc2ae26ca0ef9ecd6
baokhoa-code <khoa123456789bork@gmail.com>
2022-09-19 09:33:45 +0700

    Configure ESLint for the project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-
 const contactController = require('./controllers/contact.controller')
-const ApiError = require('./api-error');
+const ApiError = require('./api-error')
 
 const app = express()
 
@@ -10,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contact book application.' })
+    res.json({ message: 'Welcome to contact book application'})
 })
 
 app.route('/api/contacts')
@@ -25,20 +24,14 @@ app.route('/api/contacts/:id')
     .put(contactController.update)
     .delete(contactController.delete)
 
-// Handle 404 response.
 app.use((req, res, next) => {
-    // Handler for unknown route.
-    // Call next() to pass to the error handling middleware.
-    return next(new ApiError(404, 'Resource not found'));
-});
-// Define error-handling middleware last, after other app.use() and routes calls.
-app.use((err, req, res, next) => {
-        // The centralized error handling middleware.
-        // In any route handler, calling next(error)
-        // will pass to this error handling middleware.
-    return res.status(error.statusCode || 500).json({
-        message: error.message || 'Internal Server Error',
-       });
-});
+    return next(new ApiError(404, 'Resource not found'))
+})
+
+app.use((err, req, res) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    })
+})
 
 module.exports = app
